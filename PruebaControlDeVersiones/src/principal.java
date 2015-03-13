5e45a052d4aee32194761f0954c9a5705014e030
RDReplicant <david.ar1982@gmail.com>
2015-03-13 17:51:46 +0100

    Corregido

--- PruebaControlDeVersiones/src/principal.java
+++ PruebaControlDeVersiones/src/principal.java
@@ -6,7 +6,7 @@ public class principal {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("David Arochena Ruiz");
+			//David Arochena Ruiz
 	}
 
 }
